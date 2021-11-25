e5d23672f7b17498240a9f960ff7feae9d56f31c
Merbek Tursinbaev <merbektursinbaev@gmail.com>
2021-11-25 11:17:16 +0500

    25.11.2021

--- app/src/main/java/com/example/todoapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/ui/MainActivity.kt
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.example.todoapp.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
-        bottomNav.setupWithNavController(navController)
     }
 }
